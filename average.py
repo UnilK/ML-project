nan fix and average analysis

average.py
@@ -15,5 +15,17 @@ all_data.columns = ["f"+str(i) for i in range(100)] + ["vowel"]
 vowels = ["a", "e", "i", "o", "u"]
 data = [all_data[all_data["vowel"] == i] for i in vowels]
 
-print(data)
+x = [float(x)*60 for x in range(1, 101)]
+
+for i in range(5):
+
+    [inputs, labels] = np.split(data[i], [100], axis=1)
+    
+    Y = inputs.to_numpy()
+    y = np.mean(inputs.to_numpy(), axis=0)
+
+    plt.plot(x, y)
+    plt.title(vowels[i])
+    plt.show()
+    
 